fb0bc10ca399e535121233ecbd184aa35b0a7e40
Jesse Robledo <jesse@robledo.xyz>
2024-12-30 15:41:04 -0700

    added player class

---
+++ player.py
@@ -0,0 +1,9 @@
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
