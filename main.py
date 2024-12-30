5cf87a57e7b5cd02c5ce62432e72cbad24ac5866
Jesse Robledo <jesse@robledo.xyz>
2024-12-30 16:44:09 -0700

    updated main.py to draw asteroids and update field

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
+
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -12,11 +15,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
